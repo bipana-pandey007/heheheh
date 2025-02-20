b89c5e272ba15c4ac588a5cad5fca47369b5fbc1
DevBipana007 <bipanapandey754@gmail.com>
2025-02-20 20:35:06 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import ChatbotIcon from './components/ChatbotIcon'
+import ChatForm from './components/ChatForm'
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+const App = () => {
+  const[chatHistory,setChatHistory]=useState([]);
+
+  return (
+    
+      <div className="container">
+        <div className= "chatbot-popup">
+          <div className="chat-header">
+            <div className="header-info">
+              <ChatbotIcon />
+              <h2 className ="logo-text">Chatbot</h2>
+            </div>
+            <button
+             className="material-symbols-rounded">keyboard_arrow_down</button>
+ 
+          </div>
+          <div className ="chat-body"> 
+            <div className = "message bot-message">
+              <ChatbotIcon />
+              <p className="message-text">
+                hi there <br/> how can i help you !?           
+               </p>
+               {chatHistory.map((chat, index) => (
+                <ChatMessage key={index} chat={chat}/>
+
+               ))}
+            </div>
+            <div className = "message user-message">
+              <p className="message-text"> hello!! i have a question
+                         
+               </p>
+          </div>
+            
+
+        </div>
+        {/* chat footer */}
+        <div className="chat-footer">
+           <ChatForm setChatHistory={setChatHistory}/>
+      </div>
+     </div >
+    </div>
+
+  )
+}
+
+export default App
