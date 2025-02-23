d9523cca91be7f45923ee9fb47d81c16a439bf73
DevSumanP <suman121pok@gmail.com>
2025-02-23 21:47:17 +0545

    fix chat message issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,58 +4,37 @@ import ChatForm from './components/ChatForm'
 import ChatMessage from './components/ChatMessage';
 
 
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
 const App = () => {
-  const[chatHistory,setChatHistory]=useState([]);
+  const [chatHistory, setChatHistory] = useState([]);
 
   return (
-    
-      <div className="container">
-        <div className= "chatbot-popup">
-          <div className="chat-header">
-            <div className="header-info">
-              <ChatbotIcon />
-              <h2 className ="logo-text">Chatbot</h2>
-            </div>
-            <button
-             className="material-symbols-rounded">keyboard_arrow_down</button>
- 
+    <div className="container">
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
           </div>
-          <div className ="chat-body"> 
-            <div className = "message bot-message">
-              <ChatbotIcon />
-              <p className="message-text">
-                hi there <br/> how can i help you !?           
-               </p>
-               {chatHistory.map((chat, index) => (
-                <ChatMessage key={index} chat={chat}/>
-
-               ))}
-            </div>
-            <div className = "message user-message">
-              {/* <p className="message-text"> hi
-                         
-               </p> */}
-               {chatHistory.map((chat, index) => 
-                <ChatMessage key={index} chat={chat}/> )}
-               </div>
-          </div>
-            
-
+          <button className="material-symbols-rounded">keyboard_arrow_down</button>
         </div>
-        {/* chat footer */}
-        <div className="chat-footer">
-           <ChatForm setChatHistory={setChatHistory}/>
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              hi there <br /> how can i help you !?
+            </p>
+          </div>
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
+          ))}
+          <div className="message user-message"></div>
+          <div className="chat-footer">
+            <ChatForm setChatHistory={setChatHistory} />
+          </div>
+        </div>
       </div>
-     </div >
-   
-
-  )
-}
+    </div>
+  );
+};
 
 export default App
